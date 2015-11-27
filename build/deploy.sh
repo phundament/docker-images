bc5332c32f3311baae829469d9f9968f56bd0148
Tobias Munk <schmunk@usrbin.de>
2015-11-27 02:37:34 +0100

    added image tagging with stable versions

---
+++ build/deploy.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+docker login --username $REGISTRY_USER --password $REGISTRY_PASS --email $REGISTRY_EMAIL $REGISTRY_HOST
+
+# Create tagged image from git tag
+LATEST_TAG=`git describe --abbrev=0`
+export CURRENT_VERSION=`git describe`
+
+## Create tagged image
+if [ "$LATEST_TAG" = "$CURRENT_VERSION" ]; then
+    echo "Stable tag detected. ($CURRENT_VERSION)"
+    export STABLE_VERSION=1
+else
+    echo "No stable tag found. ($CURRENT_VERSION)"
+    export STABLE_VERSION=0
+fi;
+
+if [ $STABLE_VERSION = 1 ]; then
+    docker tag phundament/php-one:5.6-apache phundament/php-one:5.6-apache-${CURRENT_VERSION}
+    docker tag phundament/nginx-one:1.9 phundament/nginx-one:1.9-${CURRENT_VERSION}
+    docker tag phundament/php-one:5.6-fpm phundament/php-one:5.6-fpm-${CURRENT_VERSION}
+    docker tag phundament/php-one:7.0-fpm phundament/php-one:7.0-fpm-${CURRENT_VERSION}
+
+    docker push phundament/php-one:5.6-apache-${CURRENT_VERSION}
+    docker push phundament/nginx-one:1.9-${CURRENT_VERSION}
+    docker push phundament/php-one:5.6-fpm-${CURRENT_VERSION}
+    docker push phundament/php-one:7.0-fpm-${CURRENT_VERSION}
+    docker push phundament/php-one:5.6-${CURRENT_VERSION}
+fi;
+
+docker tag -f phundament/php-one:5.6-fpm phundament/php-one:5.6
+
+docker push phundament/php-one:5.6-apache
+docker push phundament/nginx-one:1.9
+docker push phundament/php-one:5.6-fpm
+docker push phundament/php-one:7.0-fpm
+docker push phundament/php-one:5.6
