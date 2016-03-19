0c4b44a54615d4ba5ff6247302c045f710bc8dd6
Tobias Munk <schmunk@usrbin.de>
2016-03-19 20:36:05 +0100

    updated APC test for PHP 7

--- tests/phundament/php-one/test_functions.php
+++ tests/phundament/php-one/test_functions.php
@@ -7,14 +7,23 @@
  * file that was distributed with this source code.
  */
 
+// GDlib
 if (!function_exists('imagecreatefromjpeg')) {
     echo "Error: imagecreatefromjpeg not found!\n";
     exit(1);
 }
 
-if (!function_exists('apc_add')) {
-    echo "Error: apc_add not found!\n";
-    exit(1);
+// APC
+if (version_compare(phpversion(), '7', '>=')) {
+    if (!function_exists('apcu_add')) {
+        echo "Error: apcu_add not found!\n";
+        exit(1);
+    }
+} else {
+    if (!function_exists('apc_add')) {
+        echo "Error: apc_add not found!\n";
+        exit(1);
+    }
 }
 
 
